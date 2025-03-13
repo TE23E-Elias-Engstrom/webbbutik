b0686918164b63fe6c650b69c1df3b337babacbf
Elias Engström <elias.engstrom@elev.ntig.se>
2025-03-13 14:13:53 +0100

    Skapat arrayerna i java scripten för produkterna

---
+++ script.js
@@ -0,0 +1,17 @@
+
+let produkter =
+    [
+        { id: 1, name: "GAS MAD B2-310, 3x10 tum baslåda", pris: 3489 },
+        { id: 2, name: "Bass Habit Rebel SR200M, 8 tums midbasar", pris: 398 },
+        { id: 3, name: "DLS ACW10", pris: 3490 },
+        { id: 4, name: "Pioneer SPH-EVO950DAB-UNI", pris: 11495 },
+        { id: 5, name: "Bass Habit Play SPL84", pris: 2498 },
+        { id: 6, name: "Rockford Fosgate P1-2x12, baslåda", pris: 4998 },
+
+    ]
+
+let korg = []
+
+
+
+// ========================================
